d859b92dadc1c37d13d6cab8f3e19cec1d9142cd
ms.alievaalya@gmail.com <ms.alievaalya@gmail.com>
2023-02-12 13:20:17 +0300

    123

---
+++ HM2_2/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace HM2_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonProcessStart = new System.Windows.Forms.Button();
+            this.buttonStopProcess = new System.Windows.Forms.Button();
+            this.checkBoxChooseWait = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // buttonProcessStart
+            // 
+            this.buttonProcessStart.Location = new System.Drawing.Point(12, 173);
+            this.buttonProcessStart.Name = "buttonProcessStart";
+            this.buttonProcessStart.Size = new System.Drawing.Size(203, 56);
+            this.buttonProcessStart.TabIndex = 0;
+            this.buttonProcessStart.Text = "Запустить процесс";
+            this.buttonProcessStart.UseVisualStyleBackColor = true;
+            this.buttonProcessStart.Click += new System.EventHandler(this.buttonProcessStart_Click);
+            // 
+            // buttonStopProcess
+            // 
+            this.buttonStopProcess.Location = new System.Drawing.Point(12, 12);
+            this.buttonStopProcess.Name = "buttonStopProcess";
+            this.buttonStopProcess.Size = new System.Drawing.Size(203, 65);
+            this.buttonStopProcess.TabIndex = 1;
+            this.buttonStopProcess.Text = "Принудительно завершить процесс";
+            this.buttonStopProcess.UseVisualStyleBackColor = true;
+            this.buttonStopProcess.Click += new System.EventHandler(this.buttonStopProcess_Click);
+            // 
+            // checkBoxChooseWait
+            // 
+            this.checkBoxChooseWait.AutoSize = true;
+            this.checkBoxChooseWait.Location = new System.Drawing.Point(210, 116);
+            this.checkBoxChooseWait.Name = "checkBoxChooseWait";
+            this.checkBoxChooseWait.Size = new System.Drawing.Size(258, 17);
+            this.checkBoxChooseWait.TabIndex = 2;
+            this.checkBoxChooseWait.Text = "Запустить процесс с ожиданием завершения";
+            this.checkBoxChooseWait.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 257);
+            this.Controls.Add(this.checkBoxChooseWait);
+            this.Controls.Add(this.buttonStopProcess);
+            this.Controls.Add(this.buttonProcessStart);
+            this.Name = "Form1";
+            this.Text = "Дочерний Процесс";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonProcessStart;
+        private System.Windows.Forms.Button buttonStopProcess;
+        private System.Windows.Forms.CheckBox checkBoxChooseWait;
+    }
+}
+
---
+++ HM2_2/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace HM2_2
+{
+    public partial class Form1 : Form
+    {
+        public class DllImportExample
+        {
+            [DllImport("User32.dll")]
+            public static extern int MessageBox(IntPtr hWnd, string lpText, string lpCaption, uint uType);
+        }
+        const uint MB_OK = 0x00000000;
+        const uint MB_INFORMATION = 64;
+        const uint MB_ICONERROR = 0x00000010;
+        private Process myProcess;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void buttonProcessStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                myProcess = new Process();
+                myProcess.StartInfo = new ProcessStartInfo("notepad.exe");
+                myProcess.Start();
+                if (checkBoxChooseWait.Checked)
+                {
+                    myProcess.WaitForExit();
+                    if (myProcess.HasExited)
+                    {
+                        DllImportExample.MessageBox(IntPtr.Zero, $"Процесс завершен. Код завершения: {myProcess.ExitCode}", " ", MB_OK | MB_INFORMATION);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                DllImportExample.MessageBox(IntPtr.Zero, "ERROR", "", MB_OK | MB_ICONERROR);
+            }
+        }
+
+        private void buttonStopProcess_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                myProcess.CloseMainWindow(); //убирает интерфейсную часть процесса (его главное окно)
+                myProcess.Close(); // высвобождает ресурсы выделенные на процесс
+               
+             
+            }
+            catch (Exception ex)
+            {
+                DllImportExample.MessageBox(IntPtr.Zero, "ERROR", "", MB_OK | MB_ICONERROR);
+            }
+
+
+
+        }
+    }
+}
